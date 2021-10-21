91af969b52daadec9aa8f82ed7b48758abf77ca5
Breno <you@example.com>
2021-10-21 20:23:33 -0300

    new commit

--- Patos_Online.py
+++ Patos_Online.py
@@ -10,43 +10,46 @@ for item in categorias:
 
 while True:
     try:
-        numeracao = int(input('Insira aqui a sua escolha: ' ))
+        while True:
+            numeracao = int(input('Insira aqui a sua escolha: ' )) 
+            if numeracao <= 0:
+                    print('Por favor, insira um número de 1 a 9.')
+            else:
+                break
+        categoria = categorias[numeracao-1]
     except ValueError:
         print('Por favor, insira um número de 1 a 9.')
+    except IndexError:
+        print('Por favor, insira u número de 1 a 9')
     else:
         break
-        
-categoria = categorias[numeracao-1]
 
-for item in categorias:
-        while categoria not in categorias:
-                categoria = input('Escolha uma entre as disponíveis. ')
-else:
-    if 'í' in categoria:
-       categoria = categoria.replace('í', 'i')
+
+if 'í' in categoria:
+    categoria = categoria.replace('í', 'i')
     
-    res = requests.get(f'https://www.patosonline.com/category/{categoria.lower()}/')
-    soup = BeautifulSoup(res.text, 'html.parser')
+res = requests.get(f'https://www.patosonline.com/category/{categoria.lower()}/')
+soup = BeautifulSoup(res.text, 'html.parser')
 
-    nome = soup.select('h2.post-title')
-    link = soup.select('.more-link')
+nome = soup.select('h2.post-title')
+link = soup.select('.more-link')
 
 
-    def stories_sorted(nL):
-        K = nL
-        for item in K:
-                x = item.get('title')
-                y = item.get('link')
-                print(f' \nNotícia: {x}. \n\nLink: https://www.patosonline.com{y}\n\n')
-        return 'Tenha um ótimo dia!'
+def stories_sorted(nL):
+    K = nL
+    for item in K:
+            x = item.get('title')
+            y = item.get('link')
+            print(f' \nNotícia: {x}. \n\nLink: https://www.patosonline.com{y}\n\n')
+    return 'Tenha um ótimo dia!'
 
-    def customList(nome, link):
-        L = []
-        for index, item in enumerate(nome):
-            title = nome[index].getText()
-            href = link[index].get('href', None)
-            L.append({'title' : title, 'link' : href})
-        return stories_sorted(L)
+def customList(nome, link):
+    L = []
+    for index, item in enumerate(nome):
+        title = nome[index].getText()
+        href = link[index].get('href', None)
+        L.append({'title' : title, 'link' : href})
+    return stories_sorted(L)
 
-    customList(nome, link)
+customList(nome, link)
 
