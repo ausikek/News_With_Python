7a64a6b2730aba7fd3b70f397eee6e84dcd2ce3c
ausikek <87581254+ausikek@users.noreply.github.com>
2021-10-22 14:02:28 -0300

    Update Patos_Online.py
    Changed while loop mess for fine functions

--- Patos_Online.py
+++ Patos_Online.py
@@ -1,39 +1,61 @@
-import requests
+import requests as req
 from bs4 import BeautifulSoup
 
-categorias = ['Brasil', 'Colunistas','Esportes','Locais','Gerais','Mundo','Polícial','Política','Regional']
+categorias = ['Brasil', 'Colunistas', 'Esportes', 'Locais',
+              'Gerais', 'Mundo', 'Polícial', 'Política', 'Regional']
 
 print('Olá, que tipo de notícia você deseja ver? ')
 
 for item in categorias:
     print(f'{categorias.index(item)+1}-{item}')
 
-while True:
-    try:
-        while True:
-            numeracao = int(input('Insira aqui a sua escolha: ' )) 
-            if numeracao <= 0:
-                    print('Por favor, insira um número de 1 a 9.')
-            else:
-                break
-        categoria = categorias[numeracao-1]
-    except ValueError:
-        print('Por favor, insira um número de 1 a 9.')
-    except IndexError:
-        print('Por favor, insira u número de 1 a 9')
-    else:
-        break
 
+def validate_type(value):
+    if isinstance(value, int):
+        return True
+
+    if isinstance(value, str):
+        return value.isdigit()
+
+    return False
+
+
+def validate_value_range(input):
+    return 0 < int(input) < 10
+
+
+def validate(value):
+    validated = validate_type(value) and validate_value_range(value)
+
+    return validated
+
+
+def ask_user_input():
+    valid_input = False
+    value = None
+
+    while not valid_input:
+        value = input('Insira aqui a sua escolha: ')
+        valid_input = validate(value)
+
+        if not valid_input:
+            print('Por favor, insira um número de 1 a 9.')
+
+    return int(value)
+
+
+categoria = categorias[ask_user_input()-1]
 
 if 'í' in categoria:
     categoria = categoria.replace('í', 'i')
-    
-res = requests.get(f'https://www.patosonline.com/category/{categoria.lower()}/')
+
+res = req.get(f'https://www.patosonline.com/category/{categoria.lower()}/')
 soup = BeautifulSoup(res.text, 'html.parser')
 
 nome = soup.select('h2.post-title')
 link = soup.select('.more-link')
 horas = soup.select('span.date')
+
 
 def stories_sorted(nL):
     K = nL
@@ -41,8 +63,12 @@ def stories_sorted(nL):
             x = item.get('title')
             y = item.get('link')
             z = item.get('hour')
-            print(f' \nNotícia: {x} ({z}) \n\nLink: https://www.patosonline.com{y} \n\n')
+            print(f'''
+            \nNotícia: {x} ({z})
+            \nLink: https://www.patosonline.com{y}
+            ''')
     return 'Tenha um ótimo dia!'
+
 
 def customList(nome, link, horas):
     L = []
@@ -50,8 +76,7 @@ def customList(nome, link, horas):
         title = nome[index].getText()
         hour = horas[index].getText()
         href = link[index].get('href', None)
-        L.append({'title' : title, 'link' : href, 'hour' : hour})
+        L.append({'title': title, 'link': href, 'hour': hour})
     return stories_sorted(L)
 
 customList(nome, link, horas)
-
